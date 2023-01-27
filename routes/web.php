18655fdefd7afaa84b4063b554703407dc5e6249
sagor9116 <sagor.roy9116@gmail.com>
2023-01-27 18:48:10 +0600

    completed multi-auth authentication with middleware and controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboradController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Admin related routes
+Route::group(['as' => 'admin.','prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth','admin']],function(){
+    Route::get('dashboard', [DashboradController::class, 'index'])->name('dashboard');
+});
+
+// Author related routes
+Route::group(['as' => 'author.','prefix' => 'author', 'namespace' => 'Author', 'middleware' => ['auth','author']],function(){
+    Route::get('dashboard', [DashboradController::class, 'index'])->name('dashboard');
+});
